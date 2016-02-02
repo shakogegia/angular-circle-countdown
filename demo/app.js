0704f52f9e9a46e2c46167410a1800f2dd7172cc
Gego <shalva.gegia@gmail.com>
2016-02-02 17:11:10 +0400

    Added knob

--- demo/app.js
+++ demo/app.js
@@ -1,6 +1,6 @@
 /// <reference path="../../typings/angularjs/angular.d.ts"/>
 
-var app = angular.module('App', ['circle.countdown']);
+var app = angular.module('App', ['ui.knob', 'circle.countdown']);
 
 app.controller('AppController', ['$scope', function($scope){
     
@@ -14,5 +14,82 @@ app.controller('AppController', ['$scope', function($scope){
         alert();
     }
 
+    
+    $scope.knobData = [
+		{
+			value: 30,
+			options: {
+				width: 100,
+				displayInput: false
+			}
+		},
+		{
+			value: 40,
+			options: {
+				readOnly: true,
+				width: 145,
+		        height: 145
+			}
+		},
+		{
+			value: 20,
+			options: {
+				width: 150,
+				cursor: true,
+				thickness: .3,
+				fgColor: '#222222'
+			}
+		},
+		{
+			value: 70,
+			options: {
+				fgColor: '#66CC66',
+				angleOffset: -125,
+				angleArc: 250
+			}
+		},
+		{
+			value: 90,
+			options: {
+				angleOffset: 90,
+				linecap: 'round'
+			}
+		},
+		{
+			value: 10,
+			options: {
+				width: 75,
+			    fgColor: "#ffec03",
+			    skin: "tron",
+			    thickness: .2,
+			    displayPrevious: true
+			}
+		},
+		{
+			value: 80,
+			options: {
+				displayPrevious: true,
+				min: -100	
+			}
+		}
+	];
+
+    $scope.data = 20;
+    $scope.options = {
+        width: 75,
+        fgColor: "#ffec03",
+        skin: "tron",
+        thickness: 0.2,
+        displayPrevious: true
+    }
+
+	$scope.formatOptions = function(data) {
+		data.options.max = 60;
+		data.options.width = 60;
+		data.options.thickness = 0.1;
+		// data.options.readOnly = true;
+		data.formattedOptions = JSON.stringify(data.options).replace(/,/g,"\n");
+		return data;
+	};
 
 }])
--- src/angular_circle_countdown.js
+++ src/angular_circle_countdown.js
@@ -13,13 +13,14 @@ angular.module('circle.countdown', [])
             hours: '=?hours',
             minutes: '=?minutes',
             seconds: '=?seconds',
-            time: '=?time',
+            end: '=?end',
+            now: '=?now',
             finishCallback: '&finishCallback',
         }, // {} = isolate, true = child, false/undefined = no change
         // controller: function($scope, $element, $attrs, $transclude) {},
         // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
         restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
-        template: '<input value="{{ time }}"/><input value="{{ progress }}"/>',
+        template: '<knob max="60" knob-data="data" konb-options="options"></knob>',
         // templateUrl: '',
         // replace: true,
         // transclude: true,
@@ -27,52 +28,153 @@ angular.module('circle.countdown', [])
         controller: function($scope, $element, $attrs, $transclude) {
         // link: function($scope, iElm, iAttrs, controller) {
 
-            $scope._startTime = $scope.time;
+            var timerInterval;
 
-            var timerInterval; 
+            var DaysLeft, HoursLeft, MinutesLeft, SecondsLeft;
+            var secondsLeft;
+            var isFired = false;
 
-            if (angular.isUndefined($scope.colors)) {
-                $scope.colors = {
-                    hours: "green",
-                    minutes: "green",
-                    seconds: "green",
+            secondsLeft = $scope.end - $scope.now;
+
+            $scope.secondsToDHMS = function() {
+                DaysLeft = Math.floor(secondsLeft / 86400);
+                HoursLeft = Math.floor((secondsLeft % 86400) / 3600);
+                MinutesLeft = Math.floor(((secondsLeft % 86400) % 3600) / 60);
+                SecondsLeft = Math.floor((((secondsLeft % 86400) % 3600) % 60) % 60);
+            }
+
+            $scope.doTick = function() {
+                secondsLeft--;
+                $scope.secondsToDHMS();
+                if (secondsLeft <= 0) {
+                    if (!isFired) {
+                        isFired = true;
+                        $scope.$eval($scope.finishCallback);
+                    }
+                    DaysLeft = 0;
+                    HoursLeft = 0;
+                    MinutesLeft = 0;
+                    SecondsLeft = 0;
                 }
-            }
-
-            if (angular.isUndefined($scope.autostart)) {
-                $scope.autostart = true;
-            }
-
-
-            $scope._start = function() {
                 
-                this.timerInterval = $interval( function() {
-                    if ($scope.time <= 0) {
-                        $scope._finished();
-                    };
-                    console.log( $scope.time-- );
+                // element.find('.ClassyCountdown-days input').val(365 - DaysLeft).trigger('change');
+                // element.find('.ClassyCountdown-hours input').val(24 - HoursLeft).trigger('change');
+                // element.find('.ClassyCountdown-minutes input').val(60 - MinutesLeft).trigger('change');
+                // element.find('.ClassyCountdown-seconds input').val(60 - SecondsLeft).trigger('change');
+                // element.find('.ClassyCountdown-days .ClassyCountdown-value > div').html(DaysLeft);
+                // element.find('.ClassyCountdown-hours .ClassyCountdown-value > div').html(HoursLeft);
+                // element.find('.ClassyCountdown-minutes .ClassyCountdown-value > div').html(MinutesLeft);
+                // element.find('.ClassyCountdown-seconds .ClassyCountdown-value > div').html(SecondsLeft);
 
-                    $scope.progress = ($scope.time) / $scope._startTime * 100;
+                $scope.data = 60 - SecondsLeft;
+            }
 
-                }, 1000);
+            $scope.doTick();
 
-            };
+            this.timerInterval = $interval( function() {
+                $scope.doTick();
+            }, 1000);
 
-            $scope._finished = function() {
-                $interval.cancel(this.timerInterval);
+            // function doResponsive() {
+            //     element.find('.ClassyCountdown-wrapper > div').each(function() {
+            //         $(this).css('height', $(this).width() + 'px');
+            //     });
+            //     if (settings.style.textResponsive) {
+            //         element.find('.ClassyCountdown-value').css('font-size', Math.floor(element.find('> div').eq(0).width() * settings.style.textResponsive / 10) + 'px');
+            //         element.find('.ClassyCountdown-value').each(function() {
+            //             $(this).css('margin-top', Math.floor(0 - (parseInt($(this).height()) / 2)) + 'px');
+            //         });
+            //     }
+            //     $(window).trigger('resize');
+            //     $(window).resize($.throttle(50, onResize));
+            // }
 
-                if($scope.finishCallback) {
-                    $scope.$eval($scope.finishCallback);
+            // function onResize() {
+            //     element.find('.ClassyCountdown-wrapper > div').each(function() {
+            //         $(this).css('height', $(this).width() + 'px');
+            //     });
+            //     if (settings.style.textResponsive) {
+            //         element.find('.ClassyCountdown-value').css('font-size', Math.floor(element.find('> div').eq(0).width() * settings.style.textResponsive / 10) + 'px');
+            //     }
+            //     element.find('.ClassyCountdown-value').each(function() {
+            //         $(this).css("margin-top", Math.floor(0 - (parseInt($(this).height()) / 2)) + 'px');
+            //     });
+            //     element.find('.ClassyCountdown-days input').trigger('change');
+            //     element.find('.ClassyCountdown-hours input').trigger('change');
+            //     element.find('.ClassyCountdown-minutes input').trigger('change');
+            //     element.find('.ClassyCountdown-seconds input').trigger('change');
+            // }
+
+
+            $scope.knobData = [
+                {
+                    value: 30,
+                    options: {
+                        width: 100,
+                        displayInput: false
+                    }
+                },
+                {
+                    value: 40,
+                    options: {
+                        readOnly: true,
+                        width: 145,
+                        height: 145
+                    }
+                },
+                {
+                    value: 20,
+                    options: {
+                        width: 150,
+                        cursor: true,
+                        thickness: .3,
+                        fgColor: '#222222'
+                    }
+                },
+                {
+                    value: 70,
+                    options: {
+                        fgColor: '#66CC66',
+                        angleOffset: -125,
+                        angleArc: 250
+                    }
+                },
+                {
+                    value: 90,
+                    options: {
+                        angleOffset: 90,
+                        linecap: 'round'
+                    }
+                },
+                {
+                    value: 10,
+                    options: {
+                        width: 75,
+                        fgColor: "#ffec03",
+                        skin: "tron",
+                        thickness: .2,
+                        displayPrevious: true
+                    }
+                },
+                {
+                    value: 80,
+                    options: {
+                        displayPrevious: true,
+                        min: -100   
+                    }
                 }
-            };
+            ];
 
-            if ($scope.autostart)
-                $scope._start();
-
-            $scope.$on('countdown-start', function () {
-                $scope._start();
-            });
-            
+            $scope.data = 20;
+            $scope.options = {
+                max: 60,
+                width: 75,
+                fgColor: "#ffec03",
+                skin: "tron",
+                thickness: .1,
+                displayPrevious: true
+            }
+            // ----
         }
     };
 }]);
